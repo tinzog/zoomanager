f1e8792cfc66f75721a1da79e73096492d0e897d
Christine Zoglmeier <christine.zoglmeier@stud.th-rosenheim.de>
2023-12-09 09:33:33 +0100

    [TASK] Add kotlin serialization plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.21"
     application
 }
 
@@ -12,6 +13,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
 
 tasks.test {
