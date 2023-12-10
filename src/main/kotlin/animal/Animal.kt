d6b191b6b9798d90af06c7b7257e98616236f981
Christine Zoglmeier <christine.zoglmeier@stud.th-rosenheim.de>
2023-12-10 16:34:30 +0100

    [TASK] Include gender, weight and sizegroup in animal class

--- src/main/kotlin/animal/Animal.kt
+++ src/main/kotlin/animal/Animal.kt
@@ -4,18 +4,57 @@ import kotlinx.serialization.Serializable
 import main.food.FeedingSchedule
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
+enum class Gender {
+    MALE, FEMALE, UNKNOWN
+}
+
+enum class Sizegroup {
+    SMALL, LARGE, UNKNOWN
+}
 
 @Serializable
-abstract class Animal (
+abstract class Animal(
     val name: String = "",
     val species: Species,
-    val dateOfBirth: String = "",
-    val feedingSchedule: FeedingSchedule = species.defaultFeedingSchedule
+    private var _dateOfBirth: String = "",
+    val feedingSchedule: FeedingSchedule = species.defaultFeedingSchedule,
+    val gender: Gender = Gender.UNKNOWN,
+    val weight: Double? = null
 ) {
-    override fun toString(): String {
-        return "Name: $name, Species:  ${species.toString()}"
+    var dateOfBirth: String = _dateOfBirth
+        get() = field
+        private set(value) {
+            if (!isValidDate(value)) {
+                throw IllegalArgumentException("Invalid date format. Expected format: d.M.yyyy")
+            }
+            field = value
+        }
+
+    init {
+        this.dateOfBirth = _dateOfBirth  // Validate the date format on initialization
     }
-    // todays date in string format d.m.YYYY
-    val today: String = LocalDate.now().format(DateTimeFormatter.ofPattern("d.M.yyyy"))
+
+    private fun isValidDate(dateStr: String): Boolean {
+        return try {
+            LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("d.M.yyyy"))
+            true
+        } catch (e: DateTimeParseException) {
+            false
+        }
+    }
+
+    fun getAge(): Int {
+        val birthDate = LocalDate.parse(dateOfBirth, DateTimeFormatter.ofPattern("d.M.yyyy"))
+        val today = LocalDate.now()
+        return today.year - birthDate.year - if (today.dayOfYear < birthDate.dayOfYear) 1 else 0
+    }
+
+    val sizeGroup: Sizegroup
+        get() = weight?.let { if (it < 10) Sizegroup.SMALL else Sizegroup.LARGE } ?: Sizegroup.UNKNOWN
+
+    override fun toString(): String {
+        return "Name: $name, Species: ${species.toString()}, Age: ${getAge()}, Gender: $gender, Size: $sizeGroup"
+    }
 }
