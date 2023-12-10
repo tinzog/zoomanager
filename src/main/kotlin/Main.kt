cbe7b910c160292b08bd18757b125f6df2214cec
Christine Zoglmeier <christine.zoglmeier@stud.th-rosenheim.de>
2023-12-10 16:33:42 +0100

    [TASK] Adjust main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,10 +5,25 @@ import main.zoo.Zoo
 
 fun main() {
     val zoo = Zoo()
-    zoo.restoreSpeciesList(Importer.importSpeciesFromJson(zoo))
-    println("Imported species!")
-    println("Number of species: ${zoo.speciesList.size}")
-    println("Number of foods: ${zoo.foodList.size}")
-    // print all foods with uid and name
-    zoo.foodList.forEach { println("${it.category}: ${it.name}") }
+    zoo.restoreSpeciesList(Importer.restoreZooDataFromJson(zoo))
+
+    while (true) {
+        println("1. Show all species")
+        println("2. Show all enclosures with their animals")
+        println("3. Show all foods")
+        println("4. Add or remove an animal")
+        println("5. Exit")
+        print("Enter your choice: ")
+        val choice = readLine()!!.toInt()
+        when (choice) {
+            1 -> zoo.speciesList.forEach { println(it.name) }
+            2 -> zoo.enclosureList.forEach { println(it.name) }
+            3 -> zoo.foodList.forEach { println(it.name) }
+            4 -> {
+                println("Not implemented yet")
+            }
+            5 -> return
+            else -> println("Invalid choice")
+        }
+    }
 }
