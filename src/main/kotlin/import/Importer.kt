7eee7cb0f690a012945ee1298c359c3658ee4185
Christine Zoglmeier <christine.zoglmeier@stud.th-rosenheim.de>
2023-12-10 16:32:16 +0100

    [TASK] Rename import file and reset foodList before restoring
    TODO: include all data in the initial import file

--- src/main/kotlin/import/Importer.kt
+++ src/main/kotlin/import/Importer.kt
@@ -8,14 +8,16 @@ import java.io.File
 //By declaring an object in Kotlin we define a singleton, that is, a class with only one instance. Such an instance is created lazily, the first time it’s used, in a thread-safe manner.
 object Importer {
 
-        fun importSpeciesFromJson(zoo: Zoo): List<Species> {
+        fun restoreZooDataFromJson(zoo: Zoo): List<Species> {
             zoo.speciesList.clear()
-            val jsonString = File("src/data/species.json").readText(Charsets.UTF_8)
+            zoo.foodList.clear()
+            val jsonString = File("src/data/zoo.json").readText(Charsets.UTF_8)
             val speciesList = Json.decodeFromString<List<Species>>(jsonString)
             for (species in speciesList) {
                 zoo.addSpecies(species)
                 zoo.addFood(species.getDefaultFood())
             }
+            // TODO: restore the whole zoo data: enclosures, animals, feeding schedules, foods
             return zoo.speciesList
         }
 }
